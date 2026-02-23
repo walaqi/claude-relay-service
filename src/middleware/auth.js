44754139a1678df835f4ad12298ea17796644392
sczheng189 <724100151@qq.com>
2026-02-23 21:23:38 +0800

    Revert "fix: update the limit logic, and if the daily limit is not reached after reaching the opus weekly limit, other claude models can be used"
    This reverts commit f444af49bfb0159f6aa0835b40a7049c2e4199d6.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,7 +9,7 @@ const ClientValidator = require('../validators/clientValidator')
 const ClaudeCodeValidator = require('../validators/clients/claudeCodeValidator')
 const claudeRelayConfigService = require('../services/claudeRelayConfigService')
 const { calculateWaitTimeStats } = require('../utils/statsHelper')
-const { isOpusModel } = require('../utils/modelHelper')
+const { isClaudeFamilyModel } = require('../utils/modelHelper')
 
 // 工具函数
 function sleep(ms) {
@@ -1256,7 +1256,7 @@ const authenticateApiKey = async (req, res, next) => {
       const model = requestBody.model || ''
 
       // 判断是否为 Claude 模型
-      if (isOpusModel(model)) {
+      if (isClaudeFamilyModel(model)) {
         const weeklyOpusCost = validation.keyData.weeklyOpusCost || 0
 
         if (weeklyOpusCost >= weeklyOpusCostLimit) {
