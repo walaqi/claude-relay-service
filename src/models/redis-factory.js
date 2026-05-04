8c6ac7df5d0dae02da7bdc4115587c881d02806d
walaqi <walaqi@gmail.com>
2026-05-04 10:01:12 +0800

    Delete src directory

--- src/models/redis-factory.js
+++
@@ -1,7 +0,0 @@
-const isWorkerMode = () => process.env.WORKER_MODE === 'true'
-
-if (isWorkerMode()) {
-  module.exports = require('./redis-upstash')
-} else {
-  module.exports = require('./redis')
-}
